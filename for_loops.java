8e2719a656569468370dbfcd8a2f1aadb8785025
bermar24 <joa.berriel.24@lehre.mosbach.dhbw.de>
2024-12-20 11:41:29 +0100

    This is my firs push

---
+++ for_loops.java
@@ -0,0 +1,23 @@
+public class MatrixElementWise {
+    public static void main(String[] args) {
+        int[][] A = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        int[][] B = {
+                {7, 8, 9},
+                {10, 11, 12}
+        };
+
+        int rows = A.length;
+        int cols = A[0].length;
+        int[][] C = new int[rows][cols];
+
+        // Element-wise addition
+        TODO
+
+                // Print result matrix
+                TODO
+    }
+}
